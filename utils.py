60f825aa722c5fbef49d1b5dd28fc376e8b7cf40
Christopher Galias <chris.galias@gmail.com>
2021-11-19 15:40:41 +0100

    Fix check for optimizers

--- utils.py
+++ utils.py
@@ -3,11 +3,7 @@ from types import SimpleNamespace
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from torch.optim import Adadelta as torch_adadelta
-from torch.optim import Adagrad as torch_adagrad
-from torch.optim import Adam as torch_adam
-from torch.optim import RMSprop as torch_rmsprop
-from torch.optim import SGD
+from torch.optim import Adagrad, Adam, RMSprop, SGD
 
 
 def visualize_optimizer(optim, n_steps, title=None, **params):
@@ -68,48 +64,42 @@ test_params = {
     'GradientDescent': {
         'torch_cls': SGD,
         'torch_params': {'lr': 0.1},
-        'params': {'learning_rate': 0.1}
-    },
+        'params': {'learning_rate': 0.1}},
     'Momentum': {
         'torch_cls': SGD,
         'torch_params': {'lr': 0.1, 'momentum': 0.9},
         'params': {'learning_rate': 0.1, 'gamma': 0.9}},
-    'Adagrad': {'torch_cls': torch_adagrad,
+    'Adagrad': {'torch_cls': Adagrad,
                 'torch_params': {'lr': 0.5, 'eps': 1e-8},
                 'params': {'learning_rate': 0.5, 'epsilon': 1e-8}},
-    'RMSProp': {'torch_cls': torch_rmsprop,
+    'RMSProp': {'torch_cls': RMSprop,
                 'torch_params': {'lr': 0.5, 'alpha': 0.9, 'eps': 1e-08, },
                 'params': {'learning_rate': 0.5, 'gamma': 0.9, 'epsilon': 1e-8}},
-    'Adadelta': {'torch_cls': torch_adadelta,
-                 'torch_params': {'rho': 0.9, 'eps': 1e-1},
-                 'params': {'gamma': 0.9, 'epsilon': 1e-1}},
-    'Adam': {'torch_cls': torch_adam,
+    'Adam': {'torch_cls': Adam,
              'torch_params': {'lr': 0.5, 'betas': (0.9, 0.999), 'eps': 1e-08},
              'params': {'learning_rate': 0.5, 'beta1': 0.9, 'beta2': 0.999, 'epsilon': 1e-8}}}
 
 
-def test_optimizer(optim_cls):
+def test_optimizer(optim_cls, num_steps=10):
     test_dict = test_params[optim_cls.__name__]
 
     for ns in [opt_checker_1, opt_checker_2]:
 
         torch_params = [p.clone().detach().requires_grad_(True) for p in ns.params]
-
         torch_opt = test_dict['torch_cls'](torch_params, **test_dict['torch_params'])
-        torch_opt.zero_grad()
-
-        loss = ns.f(*torch_params)
-        loss.backward()
-        torch_opt.step()
+        for _ in range(num_steps):
+            torch_opt.zero_grad()
+            loss = ns.f(*torch_params)
+            loss.backward()
+            torch_opt.step()
 
         params = [p.clone().detach().requires_grad_(True) for p in ns.params]
-
         opt = optim_cls(params, **test_dict['params'])
-        opt.zero_grad()
-
-        loss = ns.f(*params)
-        loss.backward()
-        opt.step()
+        for _ in range(num_steps):
+            opt.zero_grad()
+            loss = ns.f(*params)
+            loss.backward()
+            opt.step()
 
         for p, tp in zip(params, torch_params):
             assert torch.allclose(p, tp)
